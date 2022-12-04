88e46265c50c5a5dffae402319fbe37dfea1e856
Andres Arnedo <andresarnedo2002@gmail.com>
2022-12-04 02:49:58 -0500

    Enviroment setup

--- src/api/mail.api.js
+++ src/api/mail.api.js
@@ -1,31 +1,34 @@
 import axios from "axios";
+require("dotenv").config();
+
+const url = process.env.API_URL;
 
 export async function getMails(token) {
-  return await axios.get("http://localhost:3000/api/mail", {
+  return await axios.get(url + "/api/mail", {
     headers: { "Content-Type": "application/json", token: token },
   });
 }
 
 export async function addMail(token, data) {
-  return await axios.post("http://localhost:3000/api/mail", data, {
+  return await axios.post(url + "/api/mail", data, {
     headers: { "Content-Type": "application/json", token: token },
   });
 }
 
 export async function deleteMail(token, data) {
-  return await axios.delete(`http://localhost:3000/api/mail`,data, {
+  return await axios.delete(url + "/api/mail", data, {
     headers: { "Content-Type": "application/json", token: token },
   });
 }
 
 export async function updateMail(token, data) {
-  return await axios.put(`http://localhost:3000/api/mail`, data, {
+  return await axios.put(url + "/api/mail", data, {
     headers: { "Content-Type": "application/json", token: token },
   });
 }
 
 export async function getMail(token, { id }) {
-  return axios.get(`http://localhost:3000/api/mail/${id}`, {
+  return axios.get(`${url}/api/mail/${id}`, {
     headers: { "Content-Type": "application/json", token: token },
   });
 }
