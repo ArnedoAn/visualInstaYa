70aa81828d27f0cd689b104b9b1c56dee5d6b640
Andres Arnedo <andresarnedo2002@gmail.com>
2022-12-07 00:54:19 -0500

    Formulario de editar actualizado

--- src/components/Mail/MailForm.jsx
+++ src/components/Mail/MailForm.jsx
@@ -206,7 +206,7 @@ function MailForm({ type }) {
                         id="isFragile"
                         onChange={handleChange}
                         onSubmit={handleSubmit}
-                        defaultValue={values.package.fragile ? "Si" : "No"}
+                        defaultValue={values.package.fragile == true ? "Si" : "No"}
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       >
                         <option value={true}>Si</option>
